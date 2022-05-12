20c51c24f791eb58665da48dc70df32f78c082af
layconjohn <layconjohn@gmail.com>
2022-05-11 23:32:21 -0300

    Layout Tela 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Tela1 from "./componentes/Tela1";
 import Tela2 from "./componentes/Tela2";
 import Tela3 from "./componentes/Tela3";
+import Tela4 from "./componentes/Tela4";
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Tela1 />
             <Tela2 />
             <Tela3 />
+            <Tela4 />
         </>
     )
 }
