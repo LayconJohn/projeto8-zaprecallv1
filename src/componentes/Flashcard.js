2fa265b01ef4cdb56eb882d795c12319058d1d49
layconjohn <layconjohn@gmail.com>
2022-05-15 19:14:20 -0300

    Criação da lógica de frente/verso do flashcard

--- src/componentes/Flashcard.js
+++ src/componentes/Flashcard.js
@@ -8,85 +8,102 @@ function BotoesOpcao() {
     )
 }
 
-export default function Flashcard(props) {
+function FrenteFlashcard({texto, icone}) {
+    return (
+        <>
+            <span>{texto}</span>
+            <ion-icon name={icone}></ion-icon>
+        </>
+    )
+}
+
+function VersoFlashcard({pergunta, icone}) {
+    return (
+        <>
+            <span>{pergunta}</span>
+            <ion-icon name={icone}></ion-icon>
+        </>
+    )
+}
+
+const flashcards = [
+    {
+        texto: 'Pergunta 1',
+        pergunta: "O que é JSX?",
+        resposta: 'Uma extensão de linguagem do JavaScript',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 2',
+        pergunta: "O React é __",
+        resposta: 'uma biblioteca JavaScript para construção de interfaces',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 3',
+        pergunta: "Componentes devem iniciar com __",
+        resposta: 'letra maiúscula',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 4',
+        pergunta: "Podemos colocar __ dentro do JSX",
+        resposta: 'expressões',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 5',
+        pergunta: "O ReactDOM nos ajuda __",
+        resposta: 'interagindo com a DOM para colocar componentes React na mesma',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 6',
+        pergunta: "Usamos o npm para __",
+        resposta: 'gerenciar os pacotes necessários e suas dependências',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 7',
+        pergunta: "Usamos props para __",
+        resposta: 'passar diferentes informações para componentes',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+    {
+        texto: 'Pergunta 8',
+        pergunta: "Usamos estado (state) para __",
+        resposta: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
+        classe: "nao-respondida",
+        icone: 'play-outline',
+        selecionado: false
+    },
+];
+
+export default function Flashcard() {
     //logic
-    const flashcards = [
-        {
-            texto: 'Pergunta 1',
-            pergunta: "O que é JSX?",
-            resposta: 'Uma extensão de linguagem do JavaScript',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 2',
-            pergunta: "O React é __",
-            resposta: 'uma biblioteca JavaScript para construção de interfaces',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 3',
-            pergunta: "Componentes devem iniciar com __",
-            resposta: 'letra maiúscula',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 4',
-            pergunta: "Podemos colocar __ dentro do JSX",
-            resposta: 'expressões',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 5',
-            pergunta: "O ReactDOM nos ajuda __",
-            resposta: 'interagindo com a DOM para colocar componentes React na mesma',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 6',
-            pergunta: "Usamos o npm para __",
-            resposta: 'gerenciar os pacotes necessários e suas dependências',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 7',
-            pergunta: "Usamos props para __",
-            resposta: 'passar diferentes informações para componentes',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-        {
-            texto: 'Pergunta 8',
-            pergunta: "Usamos estado (state) para __",
-            resposta: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
-            classe: "flashcard",
-            icone: 'play-outline',
-            selecionado: false
-        },
-    ];
+
 
     //render
     return (
-
         <main className="deck">
             {flashcards.map((card, index) => {
-                return <div className={card.classe} key={index}>
-                <span>{card.texto}</span>
-                <ion-icon name={card.icone}></ion-icon>
-            </div>})}
+                return <div className={`flashcard ${card.classe}`} key={index}>
+                    {card.selecionado === false ? <FrenteFlashcard texto={card.texto} icone='play-outline' /> : <VersoFlashcard pergunta={card.pergunta} icone='repeat-outline' />}
+                </div>})}
         </main>
-
     )
 }
--- src/componentes/TelaInicial.js
+++ src/componentes/TelaInicial.js
@@ -3,15 +3,13 @@ import React from "react";
 import Tela1 from "./Tela1";
 import Tela2 from "./Tela2";
 
-export default function TelaInicial() {
+export default function TelaInicial({card}) {
     //Estado
     const [status, setStatus] = React.useState(false)
     //render
     return (
         <>
             {status ? <Tela2 />  : <Tela1 status={status} setStatus={setStatus}/>}
-            
-            
         </>
 
     )
